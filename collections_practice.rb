ade88cf1d8050939d3e170afda6748a5cc25fc18
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-26 14:14:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,17 +1,13 @@
 def sort_array_asc(numbers)
-  numbers.sort  
+  numbers.sort 
 end 
 
 def sort_array_desc(numbers)
-  numbers.sort do |a, b| 
-    b <=> a 
-  end 
+  numbers.sort.reverse 
 end 
 
 def sort_array_char_count(strings)
-     strings.sort do |a, b| 
-    a.length <=> b.length
-  end 
+    strings.sort {|left, right| left.length <=> right.length}
 end 
 
 def swap_elements(array)
@@ -20,47 +16,21 @@ def swap_elements(array)
 end
 
 def reverse_array(numbers)
-  numbers.reverse 
+  numbers.reverse
 end 
 
 def kesha_maker(strings)
-  kesha = []
-  strings.each do |third| 
-    word_array = third.split ""
-    word_array[2] = "$"
-    kesha << word_array.join
-  end 
-  kesha
+  
 end
 
 def find_a(strings)
-  strings.select do |start| 
-    start.start_with?("a")
-  end
+
 end
 
 def sum_array(array)
-  array.inject do |sum, n| sum + n 
-  end
+
 end
 
 def add_s(array)
-  array.each_with_index.collect do |a, b| 
-    if b == 1 
-      a
-    else 
-      a << "s"
-    end
-  end
+  
 end
-
-
-
-
-
-
-
-
-
-
-
